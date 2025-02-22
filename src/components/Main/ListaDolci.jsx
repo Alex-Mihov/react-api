655258db423882290631da18e3cb1223363c1378
Alex Mihov <alexmihov.as@gmail.com>
2025-02-22 11:48:16 +0100

    [feat] chiamata post

--- src/components/Main/ListaDolci.jsx
+++ src/components/Main/ListaDolci.jsx
@@ -4,8 +4,19 @@ import axios from "axios";
 
 export default function ListaDolci() {
 
+    // oggetto per il nuovo post
+    const initialForm = {
+        title: "",
+        content: "",
+        image: "",
+        tags: []
+    }
+
+    // array iniziale 
     const [lista, setLista] = useState([]);
 
+    // nuovo oggetto 
+    const [formData, setFormData] = useState(initialForm)
 
     function fetchDolci() {
 
@@ -16,19 +27,83 @@ export default function ListaDolci() {
 
     }
 
+
+    // funzione che aggiorna il formData
+    const gestioneFormData = (e) => {
+
+        const value = e.target.name === "tags" ? e.target.value.split(",") : e.target.value;
+
+        setFormData({
+            ...formData,
+            [e.target.name]: value
+        });
+
+    }
+
+    // che aggiunge un nuovo articolo alla lista
+    const aggiungiPost = (e) => {
+        e.preventDefault();
+
+        // richiesta di tipo post per aggiungere l'articolo
+        axios.post("http://localhost:3000/posts", formData)
+            .then((res) => {
+                setLista((curretPosts) => [...curretPosts, res.data])
+            })
+            .catch(err => console.log(err)
+            )
+
+        setFormData(initialForm);
+    }
+
     useEffect(fetchDolci, []);
 
     return (
         <>
+
+            <form onSubmit={aggiungiPost}>
+                <input
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={gestioneFormData}
+                    placeholder="inserisci il titolo"
+                />
+
+                <input
+                    type="text"
+                    name="image"
+                    value={formData.image}
+                    onChange={gestioneFormData}
+                    placeholder="inserisci il link dell'immagine"
+                />
+
+                <textarea
+                    name="content"
+                    value={formData.content}
+                    onChange={gestioneFormData}
+                    placeholder="inserisci il contenuto"
+                ></textarea>
+
+                <input
+                    type="text"
+                    name="tags"
+                    value={formData.tags}
+                    onChange={gestioneFormData}
+                    placeholder="inserisci tags"
+                />
+
+                <button type="submit">INVIA</button>
+            </form>
+
             {
 
                 lista.map((post) => (
 
-                    <div key={post.id}>
+                    <div className="post" key={post.id}>
                         <h2>{post.title}</h2>
                         <img src={post.image} alt={post.title} />
                         <span>{post.content}</span>
-                        <h3>{post.tags}</h3>
+                        <h5>{post.tags}</h5>
                     </div>
                 ))
             }
